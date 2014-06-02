fdceeb2af4563e7613c55f616e9b374fecb13ec3
Alessandro Pilotti <apilotti@cloudbasesolutions.com>
2014-06-03 01:12:09 +0300

    Adds pbd to output

--- BuildAutomation/BuildPCapExt.ps1
+++ BuildAutomation/BuildPCapExt.ps1
@@ -39,29 +39,33 @@ try
     $infFileName = "PCapExt.inf"
     $catFileName = "PCapExt.cat"
 
-	pushd .
-	try
-	{
-		cd $pcapExtDir
+    pushd .
+    try
+    {
+        cd $pcapExtDir
+        $buildConfig = "Win8.1 Release"
 
-        &msbuild  HyperVPCapExt.sln /p:Configuration="Win8.1 Release"
+        &msbuild  HyperVPCapExt.sln /p:Configuration="$buildConfig"
         if ($LastExitCode) { throw "MSBuild failed" }
 
-		copy -Force ".\x64\Win8.1Release\package\$sysFileName" $outputPath
-		copy -Force ".\x64\Win8.1Release\package\$infFileName" $outputPath
-	}
-	finally
-	{
-		popd
-	}
+        $buildConfigDir = $buildConfig.Replace(' ', '')
+
+        copy -Force ".\x64\$buildConfigDir\package\$sysFileName" $outputPath
+        copy -Force ".\x64\$buildConfigDir\package\$infFileName" $outputPath
+        copy -Force ".\PCapExt\x64\$buildConfigDir\PCapExt.pdb" $outputPath
+    }
+    finally
+    {
+        popd
+    }
 
     ExecRetry {
         &signtool.exe sign /sha1 $sign_cert_thumbprint /t http://timestamp.verisign.com/scripts/timstamp.dll /v "$outputPath\$sysFileName"
         if ($LastExitCode) { throw "signtool failed" }
     }
 
-	&inf2cat.exe /driver:$outputPath /os:8_x64 /USELOCALTIME
-	if ($LastExitCode) { throw "inf2cat failed" }
+    &inf2cat.exe /driver:$outputPath /os:8_x64 /USELOCALTIME
+    if ($LastExitCode) { throw "inf2cat failed" }
 
     ExecRetry {
         &signtool.exe sign /sha1 $sign_cert_thumbprint /t http://timestamp.verisign.com/scripts/timstamp.dll /v "$outputPath\$catFileName"
@@ -72,19 +76,19 @@ try
     copy -Force "$pcapExtDir\PCapExt\uninstall.cmd" $outputPath
 
     $zipPath = "$buildDir\PCapExt.zip"
-    
+
     cd $buildDir
     &7z a -r -tzip $zipPath PCapExt\*
-    
+
     $ftpsUsername = $ftpsCredentials.UserName
     $ftpsPassword = $ftpsCredentials.GetNetworkCredential().Password
 
     ExecRetry {
         &ftps -h www.cloudbase.it -ssl All -U $ftpsUsername -P $ftpsPassword -sslInvalidServerCertHandling Accept -p $zipPath /cloudbase.it/main/downloads/HyperV-PCapExt.zip
         if ($LastExitCode) { throw "ftps failed" }
-    }    
+    }
 }
 finally
 {
-	popd
+    popd
 }
